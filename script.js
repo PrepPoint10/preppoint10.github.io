c6e856e918a93c66acccdcd67cf9ec1dfe1b399e
Karamjeet singh <ks0540231@gmail.com>
2026-09-22 17:18:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -152,7 +152,7 @@ document.getElementById("logoutBtn").addEventListener("click", async () => {
     type: document.getElementById("type").value,
     description: document.getElementById("description").value.trim(),
     content: contentType === "text" ? content : null,
-    file_URL: fileUrl,
+     file_url: fileUrl,
     storage_path: storagePath
   };
 
